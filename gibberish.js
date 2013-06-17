15112aa4453deee4095c3c1e40fa7375680eb3af
clanceyp <clanceyp@gmail.com>
2013-06-17 19:45:25 +0100

    refactoring

--- gibberish.js
+++ gibberish.js
@@ -1,13 +1,13 @@
 (function(context, element){
 "use strict";
-    context.unreadable = {
+    context.gibberish = {
         blitz:function(el){
             var value = [],
                 currentVal = el.nodeValue,
                 l = currentVal.length,
                 i = 0;
             for (;i < l; i++ ){
-                value.push( unreadable.replaceChar(currentVal.charCodeAt(i)) );
+                value.push( gibberish.replaceChar(currentVal.charCodeAt(i)) );
             }
             el.nodeValue = value.join('');
         },
@@ -27,8 +27,8 @@
                 v = [97,101,105,111,117],
                 V = [65, 69, 73, 79, 85],
                 keep = [88, 90, 100, 115, 116, 120, 122],//keep X Z d s t x z
-                is = unreadable.isIn,
-                rn = unreadable.randNo;
+                is = gibberish.isIn,
+                rn = gibberish.randNo;
             if (is(code, v)){
                 chr =  String.fromCharCode( v[rn(0, 4)] );
             } else if (is(code, V)){
@@ -49,26 +49,26 @@
         randNo:function(min, max, blacklist){
             max++;// using Math.floor
             blacklist = blacklist || [];
-            var n,i=0,maxAttempts=99,is = unreadable.isIn;
+            var n,i=0,maxAttempts=99,is = gibberish.isIn;
             do {
                 i++;
                 n = Math.floor(Math.random()*(max-min)+min);
             } while (blacklist.length && is(n, blacklist) && i < maxAttempts);
             return n;
         },
-        gibberish:function(el){
+        go:function(el){
             if (el.childNodes.length > 0) {
                 for (var i = 0, l = el.childNodes.length; i<l ; i++) {
-                    unreadable.gibberish(el.childNodes[i]);
+                    gibberish.go(el.childNodes[i]);
                 }
             }
             if (el.nodeType == Node.TEXT_NODE && !!el.nodeValue) {
-                unreadable.blitz(el);
+                gibberish.blitz(el);
             }
         }
     };
     if (!!element){
-        unreadable.gibberish( element );   
+        gibberish.go( element );
     }
 
 
